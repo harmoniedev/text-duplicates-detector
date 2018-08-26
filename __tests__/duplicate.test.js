e326761df5d79a31e82124b8594340bcccdf85f8
Yehonathan Sharvit <viebel@gmail.com>
2018-08-26 22:49:04 +0300

    unskip some failing tests

--- __tests__/duplicate.test.js
+++ __tests__/duplicate.test.js
@@ -18,5 +18,3 @@ testMe.describeJsonData('duplicate text tests', testData, (t) => {
     //console.log(`duplicate pair: ${t.topic} dist: ${JSON.stringify(res.dist)} inSubject:${t.isSubject} text: ${t.a} oText: ${t.b}\nnbr.left: ${res.nbr.left.replace(/\r\n/g,' ')} res.nbrOther.left: ${res.nbrOther.left.replace(/\r\n/g,' ')} res.nbr.right: ${res.nbr.right.replace(/\r\n/g,' ')} nbrOther.right: ${res.nbrOther.right.replace(/\r\n/g,' ')}`);
     expect(res.dist.duplicate).toEqual(t.expectedDuplicate);
 });
-
-
