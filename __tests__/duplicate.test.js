3a11370288b3bea1b0fb8da3f5327de615b582b8
Yehonathan Sharvit <viebel@gmail.com>
2018-08-27 17:16:37 +0300

    refactor a bit

--- __tests__/duplicate.test.js
+++ __tests__/duplicate.test.js
@@ -7,3 +7,5 @@ testMe.describeJsonData('duplicate text tests', testData, (t) => {
     //console.log(`duplicate pair: ${t.topic} dist: ${JSON.stringify(res.dist)} inSubject:${t.isSubject} text: ${t.a} oText: ${t.b}\nnbr.left: ${res.nbr.left.replace(/\r\n/g,' ')} res.nbrOther.left: ${res.nbrOther.left.replace(/\r\n/g,' ')} res.nbr.right: ${res.nbr.right.replace(/\r\n/g,' ')} nbrOther.right: ${res.nbrOther.right.replace(/\r\n/g,' ')}`);
     expect(res).toEqual(t.expectedDuplicate);
 });
+
+
