75c0ea2a7a12233d7fd499e63913c7635c31fbce
Dekel Cohen <dekelc@harmon.ie>
2018-11-25 18:21:32 +0200

    Added babel + webpack for Nashhorn

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  entry: './sample.js',
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  }
+};
